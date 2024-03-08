a1c482daca934e245c0fb4403ea90c18df1de159
OsvaldoRuiz622 <osvaldoruiz17622@gmail.com>
2024-03-08 09:12:26 -0600

    Cambio a formulario
    Agregue Formularios y cambie los tipos de usuarios

--- PaginaWeb/Formulario/scripts.js
+++ PaginaWeb/Formulario/scripts.js
@@ -14,17 +14,19 @@ function mostrarOpciones(tipoUsuario) {
     opcionesCondicionadasDos.innerHTML = '';
 
     // Muestra las opciones condicionadas según la selección
-    if (tipoUsuario === "Personal Administrativo" || tipoUsuario === "Jefe de Departamento" || tipoUsuario === "Tecnico Docente") {
+    if (tipoUsuario === "Personal Administrativo" || tipoUsuario === "Jefe de Departamento" || tipoUsuario === "Tecnico Docente" || tipoUsuario === "Docente") {
         labelOpcionesCondicionadas.style.display = "block";
         opcionesCondicionadas.style.display = "block";
 
         // Agrega las opciones específicas según la selección
         if (tipoUsuario === "Personal Administrativo") {
-            agregarOpciones(['Direccion', 'Subdireccion Academica', 'Subdireccion de Planeacion', 'Subdireccion de Servicios Administrativos']);
+            agregarOpciones(['Direccion', 'Subdireccion Academica', 'Subdireccion de Planeacion y Vinculación', 'Subdireccion de Servicios Administrativos', 'DEPARTAMENTO DE SISTEMAS Y COMPUTACIÓN', 'DEPARTAMENTO DE METAL-MECÁNICA', 'DEPARTAMENTO DE CIENCIAS ECONÓMICO ADMINISTRATIVAS', 'DEPARTAMENTO DE INGENIERÍA INDUSTRIAL', 'DEPARTAMENTO DE DESARROLLO ACADÉMICO', 'DIVISIÓN DE ESTUDIOS PROFESIONALES', 'DIVISIÓN DE ESTUDIOS DE POSGRADO E INVESTIGACIÓN', 'DEPARTAMENTO DE CIENCIAS BÁSICAS', 'DEPARTAMENTO DE SERVICIOS ESCOLARES', 'DEPARTAMENTO DE PLANEACIÓN, PROGRAMACIÓN Y PRESUPUESTACIÓN', 'DEPARTAMENTO DE GESTIÓN TECNOLÓGICA Y VINCULACIÓN', 'DEPARTAMENTO DE ACTIVIDADES EXTRAESCOLARES', 'CENTRO DE INFORMACIÓN', 'DEPARTAMENTO DE COMUNICACIÓN Y DIFUSIÓN', 'DEPARTAMENTO DE RECURSOS HUMANOS', 'DEPARTAMENTO DE RECURSOS FINANCIEROS', 'DEPARTAMENTO DE RECURSOS MATERIALES Y DE SERVICIOS', 'DEPARTAMENTO DE MANTENIMIENTO Y EQUIPO', 'CENTRO DE CÓMPUTO']);
         } else if (tipoUsuario === "Jefe de Departamento") {
-            agregarOpciones(['Direccion', 'Subdireccion Academica', 'Subdireccion de Planeacion', 'Subdireccion de Servicios Administrativos']);
+            agregarOpciones(['Direccion', 'Subdireccion Academica', 'Subdireccion de Planeacion y Vinculación', 'Subdireccion de Servicios Administrativos', 'DEPARTAMENTO DE SISTEMAS Y COMPUTACIÓN', 'DEPARTAMENTO DE METAL-MECÁNICA', 'DEPARTAMENTO DE CIENCIAS ECONÓMICO ADMINISTRATIVAS', 'DEPARTAMENTO DE INGENIERÍA INDUSTRIAL', 'DEPARTAMENTO DE DESARROLLO ACADÉMICO', 'DIVISIÓN DE ESTUDIOS PROFESIONALES', 'DIVISIÓN DE ESTUDIOS DE POSGRADO E INVESTIGACIÓN', 'DEPARTAMENTO DE CIENCIAS BÁSICAS', 'DEPARTAMENTO DE SERVICIOS ESCOLARES', 'DEPARTAMENTO DE PLANEACIÓN, PROGRAMACIÓN Y PRESUPUESTACIÓN', 'DEPARTAMENTO DE GESTIÓN TECNOLÓGICA Y VINCULACIÓN', 'DEPARTAMENTO DE ACTIVIDADES EXTRAESCOLARES', 'CENTRO DE INFORMACIÓN', 'DEPARTAMENTO DE COMUNICACIÓN Y DIFUSIÓN', 'DEPARTAMENTO DE RECURSOS HUMANOS', 'DEPARTAMENTO DE RECURSOS FINANCIEROS', 'DEPARTAMENTO DE RECURSOS MATERIALES Y DE SERVICIOS', 'DEPARTAMENTO DE MANTENIMIENTO Y EQUIPO', 'CENTRO DE CÓMPUTO']);
         } else if (tipoUsuario === "Tecnico Docente") {
-            agregarOpciones(['Direccion', 'Subdireccion Academica', 'Subdireccion de Planeacion', 'Subdireccion de Servicios Administrativos']);
+            agregarOpciones(['DEPARTAMENTO DE CIENCIAS BÁSICAS', 'DEPARTAMENTO DE SISTEMAS Y COMPUTACIÓN', 'DEPARTAMENTO DE METAL-MECÁNICA', 'DEPARTAMENTO DE CIENCIAS ECONÓMICO ADMINISTRATIVAS', 'DEPARTAMENTO DE INGENIERÍA INDUSTRIAL', 'DEPARTAMENTO DE DESARROLLO ACADÉMICO', 'DIVISIÓN DE ESTUDIOS PROFESIONALES', 'DIVISIÓN DE ESTUDIOS DE POSGRADO E INVESTIGACIÓN']);
+        } else if (tipoUsuario === "Docente") {
+            agregarOpciones(['DEPARTAMENTO DE CIENCIAS BÁSICAS', 'DEPARTAMENTO DE SISTEMAS Y COMPUTACIÓN', 'DEPARTAMENTO DE METAL-MECÁNICA', 'DEPARTAMENTO DE CIENCIAS ECONÓMICO ADMINISTRATIVAS', 'DEPARTAMENTO DE INGENIERÍA INDUSTRIAL', 'DEPARTAMENTO DE DESARROLLO ACADÉMICO', 'DIVISIÓN DE ESTUDIOS PROFESIONALES', 'DIVISIÓN DE ESTUDIOS DE POSGRADO E INVESTIGACIÓN']);
         }
     }
 
@@ -59,7 +61,7 @@ function mostrarOpcionesDos(opcionesCondicionadas) {
             agregarOpciones(['Subdirección', 'Actividades Extraescolares', 'Centro de Información', 'Comunicación y Difusión', 'Gestion Tecnológica y Vinculación', 'Planeación Programacion y Presupuesto', 'Servicios Escolares']);
         } else if (opcionesCondicionadas === "Subdireccion de Servicios Administrativos") {
             agregarOpciones(['Subdirección', 'Centro de Cómputo', 'Mantenimiento y Equipo', 'Recursos Financieros', 'Recursos Humanos', 'Recursos Materiales y de Servicios']);
-        }
+        } 
     }
 
     function agregarOpciones(opciones) {
@@ -71,3 +73,30 @@ function mostrarOpcionesDos(opcionesCondicionadas) {
         });
     }
 }
+
+function mostrarOpcionesTipoProblema(tipoProblema) {
+    var boton = document.querySelector('button');
+    
+    // URL de redirección para cada tipo de problema
+    var redirectURL;
+    switch (tipoProblema) {
+        case 'Cuenta Institucional':
+        case 'InternetS':
+        case 'Software':
+            redirectURL = 'formularioSoftware.html';
+            break;
+        default:
+            redirectURL = 'formularioHardware.html';
+    }
+
+    // Agregar el evento de clic al botón para la redirección
+    boton.addEventListener('click', function () {
+        window.location.href = redirectURL;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tipoProblema = document.getElementById('tipoProblemaUsuario').value;
+
+    mostrarOpcionesTipoProblema(tipoProblema);
+});
