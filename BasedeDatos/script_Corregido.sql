45b73ab3c9592de16150302e23b5ef3078037d80
OsvaldoRuiz622 <osvaldoruiz17622@gmail.com>
2024-03-08 11:55:03 -0600

    Cambios
    Cambios en formulario y base de datos

--- BasedeDatos/script_Corregido.sql
+++ BasedeDatos/script_Corregido.sql
@@ -1,44 +1,52 @@
-Create database DATA
+Create database SistemaTickets
 
-USE DATA
+
+USE SistemaTickets
+
+Drop table formulario_software;
 
 Create Table solicitante (
 	id_solicitante INT PRIMARY KEY IDENTITY(1,1),
-	nombre_solicitante VARCHAR NOT NULL,
-	correo VARCHAR NOT NULL,
-    	tipo_solicitante VARCHAR NOT NULL,
-    	tipo_fallo VARCHAR NOT NULL
+	nombre_solicitante VARCHAR (50) NOT NULL,
+	correo VARCHAR (255) NOT NULL,
+    tipo_solicitante VARCHAR (50) NOT NULL,
+	area VARCHAR (50) NOT NULL,
+    tipo_fallo VARCHAR (50) NOT NULL
 );
 
 Create table formulario_software (
 	id_formulario_software INT PRIMARY KEY IDENTITY(1,1),
-	descripcion VARCHAR NOT NULL,
-	imagen VARCHAR NOT NULL,
-	id_solicitante INT NOT NULL,
-	id_operador INT NOT NULL,
-	FOREIGN KEY (id_datos) REFERENCES datos(id_solicitante),
-	FOREIGN KEY (id_operador) REFERENCES datos(id_operador)
+	descripcion VARCHAR (255) NOT NULL,
+	imagen VARCHAR (255) NOT NULL,
+	fecha_pre DATETIME  DEFAULT GETDATE(),
+	fecha_post DATE,
+	id_solicitante INT ,
+	id_operador INT ,
+	FOREIGN KEY (id_solicitante) REFERENCES solicitante(id_solicitante),
+	FOREIGN KEY (id_operador) REFERENCES Operador(id_operador)
 );
 
 Create table formulario_hardware (
 	id_formulario_hardware INT PRIMARY KEY IDENTITY(1,1),
-	tipo VARCHAR NOT NULL,
-	cantidad VARCHAR NOT NULL,
-	marca VARCHAR,
-	no_serie VARCHAR,
-	descripcion VARCHAR NOT NULL,
-	observacion_pre VARCHAR NOT NULL,
-	observacion_post VARCHAR NOT NULL,
+	cantidad VARCHAR (255),
+	marca VARCHAR (255),
+	no_serie VARCHAR (255),
+	descripcion VARCHAR (255),
+	condicion VARCHAR (255),
+	observacion_pre VARCHAR (255),
+	observacion_post VARCHAR (255),
 	fecha_pre DATETIME  DEFAULT GETDATE(),
 	fecha_post DATE,
-	id_solicitante INT NOT NULL,
-	id_operador INT NOT NULL,
-	FOREIGN KEY (id_datos) REFERENCES datos(id_solicitante),
-	FOREIGN KEY (id_operador) REFERENCES datos(id_operador)
+	id_solicitante INT,
+	id_operador INT,
+	FOREIGN KEY (id_solicitante) REFERENCES solicitante(id_solicitante),
+	FOREIGN KEY (id_operador) REFERENCES Operador(id_operador)
 );
 
 Create table Operador (
 	id_operador INT PRIMARY KEY IDENTITY(1,1),
-	nombre_operador VARCHAR	NOT NULL,
-	cargo VARCHAR NOT NULL
+	nombre_operador VARCHAR (50) NOT NULL,
+	cargo VARCHAR (50) NOT NULL
 );
+
+
--- PaginaWeb/Formulario/scripts.js
+++ PaginaWeb/Formulario/scripts.js
@@ -7,12 +7,6 @@ function mostrarOpciones(tipoUsuario) {
     opcionesCondicionadas.style.display = "none";
     opcionesCondicionadas.innerHTML = ''; // Limpiar las opciones anteriores
 
-    var labelOpcionesCondicionadasDos = document.getElementById("labelOpcionesCondicionadasDos");
-    var opcionesCondicionadasDos = document.getElementById("opcionesCondicionadasDos");
-    labelOpcionesCondicionadasDos.style.display = "none";
-    opcionesCondicionadasDos.style.display = "none";
-    opcionesCondicionadasDos.innerHTML = '';
-
     // Muestra las opciones condicionadas según la selección
     if (tipoUsuario === "Personal Administrativo" || tipoUsuario === "Jefe de Departamento" || tipoUsuario === "Tecnico Docente" || tipoUsuario === "Docente") {
         labelOpcionesCondicionadas.style.display = "block";
@@ -29,53 +23,20 @@ function mostrarOpciones(tipoUsuario) {
             agregarOpciones(['DEPARTAMENTO DE CIENCIAS BÁSICAS', 'DEPARTAMENTO DE SISTEMAS Y COMPUTACIÓN', 'DEPARTAMENTO DE METAL-MECÁNICA', 'DEPARTAMENTO DE CIENCIAS ECONÓMICO ADMINISTRATIVAS', 'DEPARTAMENTO DE INGENIERÍA INDUSTRIAL', 'DEPARTAMENTO DE DESARROLLO ACADÉMICO', 'DIVISIÓN DE ESTUDIOS PROFESIONALES', 'DIVISIÓN DE ESTUDIOS DE POSGRADO E INVESTIGACIÓN']);
         }
     }
-
-    function agregarOpciones(opciones) {
-        opciones.forEach(function (opcion) {
-            var option = document.createElement('option');
-            option.value = opcion;
-            option.textContent = opcion;
-            opcionesCondicionadas.appendChild(option);
-        });
-    }
 }
 
-function mostrarOpcionesDos(opcionesCondicionadas) {
-    var labelOpcionesCondicionadasDos = document.getElementById("labelOpcionesCondicionadasDos");
-    var opcionesCondicionadasDos = document.getElementById("opcionesCondicionadasDos");
-
-    // Reinicia y oculta las opciones condicionadas al cambiar la selección
-    labelOpcionesCondicionadasDos.style.display = "none";
-    opcionesCondicionadasDos.style.display = "none";
-    opcionesCondicionadasDos.innerHTML = ''; // Limpiar las opciones anteriores
-
-    // Muestra las opciones condicionadas según la selección
-    if (opcionesCondicionadas === "Subdireccion Academica" || opcionesCondicionadas === "Subdireccion de Planeacion" || opcionesCondicionadas === "Subdireccion de Servicios Administrativos") {
-        labelOpcionesCondicionadasDos.style.display = "block";
-        opcionesCondicionadasDos.style.display = "block";
-
-        // Agrega las opciones específicas según la selección
-        if (opcionesCondicionadas === "Subdireccion Academica") {
-            agregarOpciones(['Subdirección', 'Ciencias Basicas', 'Ciencias Economico Administrativas', 'Desarrollo Academico', 'Division de Estudios de Postgrado', 'Division de Estudios Profesionales', 'Ingenieria Industrial', 'Metal Mecanica', 'Sistemas y Computacion']);
-        } else if (opcionesCondicionadas === "Subdireccion de Planeacion") {
-            agregarOpciones(['Subdirección', 'Actividades Extraescolares', 'Centro de Información', 'Comunicación y Difusión', 'Gestion Tecnológica y Vinculación', 'Planeación Programacion y Presupuesto', 'Servicios Escolares']);
-        } else if (opcionesCondicionadas === "Subdireccion de Servicios Administrativos") {
-            agregarOpciones(['Subdirección', 'Centro de Cómputo', 'Mantenimiento y Equipo', 'Recursos Financieros', 'Recursos Humanos', 'Recursos Materiales y de Servicios']);
-        } 
-    }
-
-    function agregarOpciones(opciones) {
-        opciones.forEach(function (opcion) {
-            var option = document.createElement('option');
-            option.value = opcion;
-            option.textContent = opcion;
-            opcionesCondicionadasDos.appendChild(option);
-        });
-    }
+function agregarOpciones(opciones) {
+    opciones.forEach(function (opcion) {
+        var option = document.createElement('option');
+        option.value = opcion;
+        option.textContent = opcion;
+        opcionesCondicionadas.appendChild(option);
+    });
 }
 
-function mostrarOpcionesTipoProblema(tipoProblema) {
-    var boton = document.querySelector('button');
+
+function mostrarOpcionesTipoProblema(tipoProblema, botonId) {
+    var boton = document.getElementById(botonId);
     
     // URL de redirección para cada tipo de problema
     var redirectURL;
@@ -98,5 +59,5 @@ function mostrarOpcionesTipoProblema(tipoProblema) {
 document.addEventListener('DOMContentLoaded', function () {
     var tipoProblema = document.getElementById('tipoProblemaUsuario').value;
 
-    mostrarOpcionesTipoProblema(tipoProblema);
+    mostrarOpcionesTipoProblema(tipoProblema, 'btnSiguiente');
 });
